1f7316ad010754928328d81d775805204adfdd1c
2nil <casalalanne@free.fr>
2024-05-06 17:23:54 +0200

    Checked in by casal from PC-Denis on Mon May  6 17:23:54 CEST 2024

--- wv-backup/head.js
+++ wv-backup/head.js
@@ -107,24 +107,22 @@ function save_conf_and_exit() {
   webapp_exit();
 }
 
-
-var backupCount=0;
-var backupTable;
-function addTdObj(tr, obj) {
+function addTdObjres(tr, obj) {
   var td = document.createElement("td");
   obj.style="display: block;";
   td.appendChild(obj);
   tr.appendChild(td);
 }
 
-function addTdLabel(tr, forId, txt) {
+function addTdLabelres(tr, forId, txt) {
   var lab = document.createElement("label");
   lab.htmlFor=forId;
   lab.innerHTML=txt;
   addTdObj(tr, lab);
 }
 
-class BackupElement extends HTMLElement {
+
+class BakLn extends HTMLElement {
   static observedAttributes = ["src", "dst"];
 
   constructor() {
@@ -132,42 +130,69 @@ class BackupElement extends HTMLElement {
   }
 
   create=function() {
-    // Create checkbox
-    var tr = document.createElement("tr");
-    tr.style="outline:thin solid; user-select: none";
+    var backupTable;
 
-    var cbox = document.createElement("input");
-    cbox.type = "checkbox";
-    cbox.id = "bakCB"+backupCount;
-    addTdObj(tr, cbox);
-
-    // Create label for src
-    addTdLabel(tr, cbox.id, this.getAttribute("src"));
-    // Create label for arrow
-    addTdLabel(tr, cbox.id, "&#x1F449;");
-    // Create label for dst
-    addTdLabel(tr, cbox.id, this.getAttribute("dst"));
-
-    if (backupCount === 0) {
+    // Créé une nouvelle table si le 1er enfant du père n'en est pas une.
+    if (this.parentNode.firstChild.tagName === undefined || this.parentNode.firstChild.tagName.toLowerCase() !== 'table') {
+      console.log("Creating a new table");
       backupTable=document.createElement("table");
       backupTable.style="border-collapse:separate; border-spacing: 4px 4px;";
-      this.appendChild(backupTable);
-    }
-    backupTable.appendChild(tr);
-    backupCount++;
+      backupTable.dataset.cbUniqPfx=Date.now().toString(36);
+      backupTable.dataset.cbIdCount=0;
+      this.parentNode.prepend(backupTable);
+    } else backupTable=this.parentNode.firstChild;
 
-    console.log(`${backupCount},${this.getAttribute("src")},${this.getAttribute("dst")}`);
+    // Add a row
+    console.log(Date.now().toString(36)+", "+Date.now().valueOf()+", "+new Date().valueOf());
+    var tr=this.parentNode.firstChild.insertRow(-1), cbId=Math.random().toString(16).slice(2);
+    cbId=backupTable.dataset.cbUniqPfx+'.'+backupTable.dataset.cbIdCount.toString();
+    console.log(`cbId: ${cbId}`);
+    backupTable.dataset.cbIdCount++;
+
+    tr.style="outline:thin solid; user-select: none";
+
+    function addTdObj(obj) {
+      var td = document.createElement("td");
+      obj.style="display: block;"; // Pour étirer l'objet sur toute la largeurde la cellule
+      td.appendChild(obj);
+      tr.appendChild(td);
+    }
+
+    function addTdLabel(txt) {
+      var lab = document.createElement("label");
+      lab.htmlFor=cbId;
+      lab.innerHTML=txt;
+      addTdObj(lab);
+    }
+
+    // Create a checkbox as the first cell of the row
+    var cbox = document.createElement("input");
+    cbox.type = "checkbox";
+    cbox.id = cbId;
+    addTdObj(cbox);
+    console.log(`${this.tagName}[${cbox.id}]:[${this.innerHTML}]`);
+    this.innerHTML='';
+
+    // Add a second cell for the src of the backup
+    addTdLabel(this.getAttribute("src"));
+    // Create a third cell as a decoration
+    addTdLabel("&#x1F449;");
+    // Add a fourth cell for the dst of the backup
+    addTdLabel(this.getAttribute("dst"));
   }
 
   connectedCallback() {
-    this.create();
+//    this.create();
+    addEventListener("load", (event) => {
+      this.create();
+    });
   }
 }
+customElements.define("bak-ln", BakLn);
 
-customElements.define("backup-element", BackupElement);
 
 function list_available_backup () {
-  const allBak = document.getElementsByTagName("backup-element");
+  const allBak = document.getElementsByTagName("bak");
   for (i=0; i < allBak.length; i++) {
     console.log("SRC: "+allBak.item(i).getAttribute("src")+", DST: "+allBak.item(i).getAttribute("dst"));
   }
